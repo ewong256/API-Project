0b3e9560f80da9ea3ecd179180d781c4dd3f3a29
Ethan Wong <wngethn256@gmail.com>
2024-05-19 20:22:13 -0400

    review image bugfix

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -9,34 +9,30 @@ const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router()
 
 // Delete a reviewImage
-router.delete('/:imageId', requireAuth, async(req, res, next) => {
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
+    const deleteImage = await SpotImage.findByPk(req.params.imageId);
 
-    const userId = req.user.userId
-    const imageId = req.params.imageId
-
-    const reviewImage = await ReviewImage.findByPk(imageId, { include: { model: Review } })
-
-
-    if(!reviewImage) {
-        const err = new Error('Resource not found')
-        err.status = 404
-        err.title = 'Resource not found'
-        err.errors = { message: 'Image could not be found'}
-        return next(err)
+    if (!deleteImage) {
+      err.status = 404
+      const err = new Error('Resource not found')
+      err.title = 'Resource not found'
+      err.errors = { message: 'Image could not be found'}
+      return next(err)
+    }
+    if (deleteImage.userId !== req.user.id) {
+      const err = new Error('Must own the image to delete')
+      err.status = 403
+      err.title = 'Forbidden'
+      err.errors= { message: 'Unauthorized for this action'}
+      return next(err);
     }
 
-    if(userId !== reviewImage.Review.userId) {
-        const err = new Error('Must own the image to delete')
-        err.status = 403
-        err.title = 'Forbidden'
-        err.errors= { message: 'Unauthorized for this action'}
-        return next(err)
-    }
+    await deleteImage.destroy()
 
-    await reviewImage.destroy()
-    res.status(200).json({ message: 'Successfully deleted' })
-})
+    res.status(200).json({ message: "Successfully deleted" })
+  })
 
+  module.exports = router;
 
 
 module.exports = router
