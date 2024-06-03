09fa505d7fa79e3c70fa3fe9e7f533f4b5f2df79
Ethan Wong <wngethn256@gmail.com>
2024-06-03 18:23:17 -0400

    action types defined

---
+++ frontend/src/store/spots.js
@@ -0,0 +1,13 @@
+import { csrfFetch } from "./csrf";
+
+// actions
+const GET_SPOTS = 'spots/getSpots'
+const ADD_SPOT = 'spots/addSpot'
+const REMOVE_SPOT = 'spots/removeSpot'
+const LOAD_SPOT_ID = 'spots/loadSpotId'
+const EDIT_SPOT = 'spots/editSpot'
+
+// action creators
+const getSpots = (spots) => ({
+    
+})
